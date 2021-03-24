da539b55f3be7496de432567a9a5b62624e42cf4
Lucas Stellet <lustepe@gmail.com>
2021-03-24 00:08:23 -0300

    add user repository

---
+++ server/application/repositories/user.repository.go
@@ -0,0 +1,41 @@
+package repositories
+
+import (
+	"fmt"
+
+	"github.com/lucas-stellet/ctsf-admin/server/domain"
+	"gorm.io/gorm"
+)
+
+type UserRepository interface {
+	Insert(user *domain.User) (*domain.User, error)
+	Find(email string) (*domain.User, error)
+}
+
+type UserRepositoryDb struct {
+	Db *gorm.DB
+}
+
+func (repo UserRepositoryDb) Insert(user *domain.User) (*domain.User, error) {
+	err := repo.Db.Create(user).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (repo UserRepositoryDb) Find(email string) (*domain.User, error) {
+
+	var user domain.User
+
+	repo.Db.First(&user, "email = ?", email)
+
+	if user.Token == "" {
+		return nil, fmt.Errorf("User does not exist")
+	}
+
+	return &user, nil
+
+}
