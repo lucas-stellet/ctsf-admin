ddade9440513d12ae6d7c17a6ba91867375a6c5b
Lucas Stellet <lustepe@gmail.com>
2021-03-25 10:17:41 -0300

    change init function and env path

--- server/framework/cmd/server/main.go
+++ server/framework/cmd/server/main.go
@@ -1,20 +1,37 @@
 package main
 
 import (
-	"log"
-	"os"
-
+	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/lucas-stellet/ctsf-admin/server/application/routes"
+	"github.com/lucas-stellet/ctsf-admin/server/framework/utils"
+	"log"
+	"path"
+	"runtime"
+
+	"os"
 )
 
 func init() {
-	err := godotenv.Load()
+	_, b, _, _ := runtime.Caller(0)
+	d := path.Join(path.Dir(b), "../../../")
+
+	err := godotenv.Load(d + "/.env")
 
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatalf("Error loading .env files: %v", err)
 	}
 }
 
 func main() {
 	db := utils.ConnectDB(os.Getenv("env"))
+
+	app := fiber.New()
+
+	userRouter := app.Group("/users")
+
+	userRouter.Post("/", routes.CreateUserRoute(db))
+
+	app.Listen(":3000")
 }
